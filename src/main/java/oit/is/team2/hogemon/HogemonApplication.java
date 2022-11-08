1cd06180e27b483a388207c0133faaa35bc6f9fd
e1b20111 <e1b20111@st.oit.ac.jp>
2022-11-08 15:17:46 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team2/hogemon/HogemonApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team2.hogemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team2/hogemon/HogemonApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.hogemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
