7d6c58ab11a19c7805f3d7bc3b36ffeb02a60264
e1b20103 <e1b20103@st.oit.ac.jp>
2022-11-29 15:26:35 +0900

    試合結果の表示

---
+++ src/main/java/oit/is/team2/hogemon/model/result.java
@@ -0,0 +1,33 @@
+package oit.is.team2.hogemon.model;
+
+public class result {
+
+  String usermonsterName;
+  String enemymonsterName;
+  String matchresult;
+
+  public String getUsermonsterName() {
+    return usermonsterName;
+  }
+
+  public void setUsermonsterName(String usermonsterName) {
+    this.usermonsterName = usermonsterName;
+  }
+
+  public String getEnemymonsterName() {
+    return enemymonsterName;
+  }
+
+  public void setEnemymonsterName(String enemymonsterName) {
+    this.enemymonsterName = enemymonsterName;
+  }
+
+  public String getMatchresult() {
+    return matchresult;
+  }
+
+  public void setMatchresult(String matchresult) {
+    this.matchresult = matchresult;
+  }
+
+}
