85e2b1d9a757c7b4c57b74fffdd8293f88f6e972
e1b20062T <e1b20062@oit.ac.jp>
2022-12-13 15:31:16 +0900

    戦闘画面における対戦相手の表示

---
+++ src/main/java/oit/is/team2/hogemon/model/Skill.java
@@ -0,0 +1,20 @@
+package oit.is.team2.hogemon.model;
+
+public class Skill {
+  String skillName;
+  int damage;
+
+  public String getSkillName() {
+    return skillName;
+  }
+  public void setSkillName(String skillName) {
+    this.skillName = skillName;
+  }
+  public int getDamage() {
+    return damage;
+  }
+  public void setDamage(int damage) {
+    this.damage = damage;
+  }
+
+}
