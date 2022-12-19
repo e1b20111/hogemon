55fa1963ef210f8ed115e041aa840a53804c5c9b
e1b20111 <e1b20111@st.oit.ac.jp>
2022-12-19 14:34:14 +0900

    試合終了判定と試合結果登録の処理

--- src/main/java/oit/is/team2/hogemon/model/resultMapper.java
+++ src/main/java/oit/is/team2/hogemon/model/resultMapper.java
@@ -2,7 +2,7 @@ package oit.is.team2.hogemon.model;
 
 import java.util.ArrayList;
 
-//import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 //import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
@@ -12,5 +12,11 @@ public interface resultMapper {
 
   @Select("SELECT * from result;")
   ArrayList<result> selectAllresults();
+
+  @Select("SELECT * from result DESC LIMIT 1;")
+  result selectLastresult();
+
+  @Insert("INSERT INTO result (mymonstername,enemymonstername,matchresult) VALUES (#{mymonsterName},#{enemymonsterName},#{matchresult});")
+  void insertResult(result result);
 
 }
